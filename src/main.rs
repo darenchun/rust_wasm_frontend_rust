8f020f5713118513acad7b60d83778319d7ebba7
darenchun <darenchun@naver.com>
2022-07-25 14:38:19 +0900

    불필요한 자원 삭제

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod pages;
 mod router;
-mod cookies;
 use yew::prelude::*;
 use yew::{html};
 use yew_router::prelude::*;
@@ -8,15 +7,15 @@ use yew_router::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     /* set application to utilize "cookies". */
-
-
+    
+    /* run!! */
     return html! {
-       html! {
-           <BrowserRouter>
-               <Switch<router::Route> render={Switch::render(router::switch)} />
-           </BrowserRouter>
-       }
-       };
+        html! {
+            <BrowserRouter>
+            <Switch<router::Route> render={Switch::render(router::switch)} />
+            </BrowserRouter>
+        }
+    };
 }
 
 fn main() {
